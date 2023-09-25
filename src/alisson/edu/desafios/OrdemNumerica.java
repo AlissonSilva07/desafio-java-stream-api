a3d0dffbcdcc19096abb1e01568fd856f9218ab9
Alisson Rego <alissondroid07@gmail.com>
2023-09-25 09:21:33 -0300

    Resolvido o primeiro desafio

--- src/alisson/edu/desafios/OrdemNumerica.java
+++ src/alisson/edu/desafios/OrdemNumerica.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrdemNumerica {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        List<Integer> numerosPorOrdem = new ArrayList<>();
+        List<Integer> numerosPorOrdem = numeros.stream().sorted().toList();
+        numerosPorOrdem.forEach(System.out::println);
     }
 }
